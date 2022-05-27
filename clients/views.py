21c237938f4367234b5f0fa483ac374a1fd0bcf7
mkindia <manojkumarindia@yahoo.com>
2022-05-27 20:46:34 +0530

    27-05-22

--- clients/views.py
+++ clients/views.py
@@ -1,7 +1,7 @@
 
 
 from django.shortcuts import redirect, render
-from django.http import JsonResponse , HttpResponseRedirect
+from django.http import JsonResponse 
 from django.conf import settings
 from django.core.mail import send_mail
 from django.views.decorators.cache import cache_control
--- orders/models.py
+++ orders/models.py
@@ -34,16 +34,16 @@ class Item_Order(models.Model):
 
 
 class Item_Order_Transfer(models.Model):
-    client_id=models.ForeignKey(Client,on_delete=models.CASCADE)
-    consignee_id=models.ForeignKey(Consignee,on_delete=models.CASCADE)
+    #client_id=models.ForeignKey(Client,on_delete=models.CASCADE)
+    #consignee_id=models.ForeignKey(Consignee,on_delete=models.CASCADE)
     item_order_id = models.ForeignKey(Item_Order,on_delete=models.CASCADE)  
     date=models.DateField()
-    item_name = LowerCase(max_length=200)
-    item_variant = LowerCase(max_length=200)
-    item_price = models.DecimalField(default=1.1,max_digits=7,decimal_places=2)
-    price_per_unit = LowerCase(max_length=200)   
+    #item_name = LowerCase(max_length=200)
+    #item_variant = LowerCase(max_length=200)
+    #item_price = models.DecimalField(default=1.1,max_digits=7,decimal_places=2)
+    #price_per_unit = LowerCase(max_length=200)   
     item_qty = models.DecimalField(default=0,max_digits=7,decimal_places=3)
-    item_unit = LowerCase(max_length=200)
+    #item_unit = LowerCase(max_length=200)
     create_date=models.DateField(auto_now_add=True,null=True,blank=True)
     comment = LowerCase(max_length=200,null=True,blank=True)
 
@@ -52,17 +52,17 @@ class Item_Order_Status(models.Model):
                 ('dispatched','dispatched'),
                 ('transferred','transferred'),]
 
-    client_id=models.ForeignKey(Client,on_delete=models.CASCADE)
-    consignee_id=models.ForeignKey(Consignee,on_delete=models.CASCADE)  
+    #client_id=models.ForeignKey(Client,on_delete=models.CASCADE)
+    #consignee_id=models.ForeignKey(Consignee,on_delete=models.CASCADE)  
     item_order_id = models.ForeignKey(Item_Order,on_delete=models.CASCADE)
     transfer_id = models.ForeignKey(Item_Order_Transfer,on_delete=models.CASCADE,blank=True,null=True)     
     date=models.DateField()
-    item_name = LowerCase(max_length=200)
-    item_variant = LowerCase(max_length=200)
-    item_price = models.DecimalField(default=1.1,max_digits=7,decimal_places=2)
-    price_per_unit = LowerCase(max_length=200)   
+    #item_name = LowerCase(max_length=200)
+    #item_variant = LowerCase(max_length=200)
+    #item_price = models.DecimalField(default=1.1,max_digits=7,decimal_places=2)
+    #price_per_unit = LowerCase(max_length=200)   
     item_qty = models.DecimalField(default=0,max_digits=7,decimal_places=3)
-    item_unit = LowerCase(max_length=200)
+    #item_unit = LowerCase(max_length=200)
     create_date=models.DateField(auto_now_add=True,null=True,blank=True)
     status = LowerCase(max_length=30,blank=True, null=True, choices=order_status,default='dispatched',)
     comment = LowerCase(max_length=200,null=True,blank=True)
